d0f607ec06b3a99819dc125cabf1a55ee392f9da
chukwudi <nwaforcs@edutams.net>
2025-08-04 14:56:45 +0100

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: [
+      'chukwudi-nwafor-lendsqr-fe-test.onrender.com', // Render domain
+      'localhost'
+      // add custom domains here if you plan to connect one later
+    ]
+  }
 })
